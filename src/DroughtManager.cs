0da84d423066a3b0259dcf55703ff8196df0a0cb
Krystian Ziarko <krystian.ziarko@gmail.com>
2021-12-08 19:08:28 +0100

    initial

---
+++ src/DroughtManager.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using TimberbornAPI;
+using UnityEngine;
+
+namespace DroughtManager
+{
+    [BepInPlugin("jimmzexe.timberborn.drougthmanager", "DroughtManager", "1.0.0")]
+    [BepInDependency("com.timberapi.timberapi")]
+    [HarmonyPatch]
+    public class DroughtManager : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Logger.LogInfo("Plugin DroughtManager is loaded!");
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+            // TimberAPI.DependecyRegistry.AddConfigurator();
+        }
+    }
+}
